67587c1bb43b217bbb92187b5ccdeee76c829575
Touch <s.chantouch@mango-byte.com>
2021-06-25 23:42:36 +0700

    chore: :tada: fix types

--- types/index.d.ts
+++ types/index.d.ts
@@ -4,28 +4,12 @@ interface CopyTextInterface {
     trigger: String | HTMLElement | HTMLCollection | NodeList,
     clearSelection: () => void
 }
-declare module '@nuxt/types' {
-    interface Context {
-        $clipboardConfig: {
-            autoSetContainer: boolean,
-            appendToBody: boolean
-        }
-        $copyText(text: string, container?: object | HTMLElement): Promise<CopyTextInterface>
-    }
-    interface NuxtAppOptions {
-        $clipboardConfig: {
-            autoSetContainer: boolean,
-            appendToBody: boolean
-        }
-        $copyText(text: string, container?: object | HTMLElement): Promise<CopyTextInterface>
-    }
-}
+
 declare module 'vue/types/vue' {
     interface Vue {
-        $clipboardConfig: {
-            autoSetContainer: boolean,
-            appendToBody: boolean
-        }
-        $copyText(text: string, container?: object | HTMLElement): Promise<CopyTextInterface>;
+        $copyText: (text: any, container?: object | HTMLElement) => Promise<CopyTextInterface>;
+    }
+    interface VueConstructor {
+        $copyText: (text: any, container?: object | HTMLElement) => Promise<CopyTextInterface>;
     }
 }
